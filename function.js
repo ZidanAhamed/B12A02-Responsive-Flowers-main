bb90df08266b36442483690855e1f9197cd89161
ZidanAhamed <zidanahamed2002@gmail.com>
2026-05-05 22:19:17 +0600

    Done the js assignment

---
+++ function.js
@@ -0,0 +1,7 @@
+function Divider(input1, input2){
+    var division = input1 / input2;
+    console.log("The division is = " + division);
+    return division;
+}
+const result = Divider(5f00,5);
+console.log(result);
---
+++ practice.js
@@ -0,0 +1,44 @@
+// problem 1
+var area = 10000;
+// var divided = area / 2;
+
+// console.log(divided);
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+
+// money >= 25000 ? console.log('Laptop'): money >= 10000 ? console.log('Cycle'): console.log('Chocolate')
+
+
+// /** Problem -03 ( Medicine Planner ) */
+// var lastDay = 30 ;
+// //write your code here
+// for(var day =1; day<= lastDay; day++){
+//     if(day % 4 === 0){
+//         console.log(day + " - Medicine");  
+//     }
+//     else{
+//         console.log(day + " - Rest");
+//     }
+// }
+
+// /** Problem 05 - ( PH Email Generator )  */
+// var student= { name: "jhankar" , roll: 1014 ,department: "cse" };
+// var student2= { name: "monu" , roll: 99 ,department: "eee" };
+// var student3= { name: "meow" , roll: 96 ,department: "cse" };
+// //write your code here
+// var email = (student.name) + (student.roll) + (".") + (student.department) + ("@ph.ac.bd")
+// var email2 = (student2.name) + (student2.roll) + (".") + (student2.department) + ("@ph.ac.bd")
+// var email3 = (student3.name) + (student3.roll) + (".") + (student3.department) + ("@ph.ac.bd")
+// console.log(email + '\n' + email2 + '\n' + email3);
+
+// /** Problem 06 :  (Current Salary )  */
+// var experience = 40;
+// var startingSalary = 30000;
+// //write your code here
+// var presentSalary = startingSalary * (1.05 ** experience);
+// console.log(presentSalary.toFixed(2));
+
+
+
